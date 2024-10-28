e86f94a08b16034aad7141ee745ffb34449c5972
Dhuruv Bansal <Dhuruvbansal53@gmail.com>
2024-10-28 16:17:54 +0530

    D

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,12 +1,8 @@
-import React, { useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
 
 const PageNotFound = () => {
-    const navlink = useNavigate()
-    useEffect(() => {
-        navlink("auth/home")
-    })
+
     return (
         <div>
             wrong turn go :
